24c01a8518892ca3fde04bcc229279ccdfae5aca
Precious Betine <preciousbetine@gmail.com>
2023-03-13 16:34:35 +0100

    Build project

--- dist/main.js
+++ dist/main.js
@@ -116,7 +116,7 @@ eval("\n\n/* istanbul ignore next  */\nfunction styleTagTransform(css, styleElem
   \**********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _style_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./style.css */ \"./src/style.css\");\n// import _ from 'lodash';\n\n\n\n//# sourceURL=webpack://module-2-capstone-project/./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _style_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./style.css */ \"./src/style.css\");\n// import _ from 'lodash';\r\n\r\n\n\n//# sourceURL=webpack://module-2-capstone-project/./src/index.js?");
 
 /***/ })
 
