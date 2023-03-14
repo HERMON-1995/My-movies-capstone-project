48cc1e127cfaba7f5ec1ba09b289d6fc7a573dfe
Precious Betine <preciousbetine@gmail.com>
2023-03-14 20:37:52 +0100

    Display movies on homepage

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
-// import _ from 'lodash';
 import './style.css';
+import { getSelectedShows } from './modules/tvmaze';
+import { generateMovieTile } from './modules/movies';
+
+const initialize = async () => {
+  const selectedShows = await getSelectedShows();
+  const moviesContainer = document.querySelector('main');
+
+  selectedShows.forEach((show) => {
+    const movieTile = generateMovieTile(show);
+    moviesContainer.appendChild(movieTile);
+  });
+};
+
+initialize();
---
+++ src/modules/tvmaze.js
@@ -0,0 +1,20 @@
+const baseUrl = 'https://api.tvmaze.com';
+
+const shows = [270, 245, 650, 840, 1220, 1200];
+
+const getShow = async (id) => {
+  const response = await fetch(`${baseUrl}/shows/${id}`);
+  const show = await response.json();
+  return show;
+};
+
+const getSelectedShows = async () => {
+  const showsData = [];
+  for (let i = 0; i < shows.length; i++) {
+    const show = await getShow(shows[i]);
+    showsData.push(show);
+  }
+  return showsData;
+};
+
+export { getSelectedShows };
