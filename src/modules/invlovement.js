6e1fc33baf85ff0f348af15ceb8daa3b575a9f39
Precious Betine <preciousbetine@gmail.com>
2023-03-15 14:35:43 +0100

    Fix eslint errors

--- src/modules/invlovement.js
+++ src/modules/invlovement.js
@@ -1,15 +1,15 @@
-const baseUrl = "https://us-central1-involvement-api.cloudfunctions.net/capstoneApi";
-const appID = "NPgV5E1zHcIqM2oujsnW";
+const baseUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi';
+const appID = 'NPgV5E1zHcIqM2oujsnW';
 
 const getLikes = async () => {
   const response = await fetch(`${baseUrl}/apps/${appID}/likes`);
   const data = await response.text();
   return data;
-}
+};
 
 const getMovieLikes = async (id) => {
   let allLikes = await getLikes();
-  allLikes ||= "[]";
+  allLikes ||= '[]';
   allLikes = JSON.parse(allLikes);
 
   const like = allLikes.find((like) => like.item_id === id);
@@ -17,13 +17,13 @@ const getMovieLikes = async (id) => {
     return like.likes;
   }
   return 0;
-}
+};
 
 const addLike = async (id) => {
   const response = await fetch(`${baseUrl}/apps/${appID}/likes`, {
-    method: "POST",
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify({
       item_id: id,
@@ -31,7 +31,7 @@ const addLike = async (id) => {
   });
   const data = await response.text();
   return data;
-}
+};
 
 export {
   addLike,
